6c8d516d6814dde286978fdd3d22141eccfc6da9
RinxSR <rinxsr@gmail.com>
2016-02-14 12:26:59 +0300

    test new settings

--- core/src/com/seabattle/game/Main.java
+++ core/src/com/seabattle/game/Main.java
@@ -3,23 +3,26 @@ package com.seabattle.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
+	Texture field;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		field = new Texture("FullWater.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.4f, 0.4f, 0.4f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 
-
+		batch.draw(field, 30, 40);
 
 		batch.end();
 	}
